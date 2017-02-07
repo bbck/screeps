a49db78381b7a0ef42475a3df4e1c52cc2ccb519
Chris Boyle <chris@bbck.net>
2017-02-07 16:49:49 -0500

    Ignore dropped energy for now

--- role.upgrader.js
+++ role.upgrader.js
@@ -17,17 +17,10 @@ var roleUpgrader = {
                 creep.moveTo(creep.room.controller);
             }
         } else {
-          var droppedEnergy = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
-          if(droppedEnergy) {
-            if(creep.pickup(droppedEnergy) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(droppedEnergy);
-            }
-          } else {
             var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
               creep.moveTo(source);
             }
-          }
         }
     }
 };
