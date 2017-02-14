322a86bead3f4ea863f11be059393fb02c3fe76f
Chris Boyle <chris.boyle@enernoc.com>
2017-02-14 17:31:47 -0500

    Take spare energy from storage first

--- src/prototype.creep.js
+++ src/prototype.creep.js
@@ -9,15 +9,15 @@ module.exports = function() {
                      s.store[RESOURCE_ENERGY] > 0
     });
 
-    // Get energy from containers
-    if (containersWithEnergy) {
-      if (this.withdraw(containersWithEnergy, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        this.moveTo(containersWithEnergy);
-      }
-    // Otherwise get some from storage
-    } else if (storageWithEnergy.length) {
+    // Get energy from storage
+    if (storageWithEnergy.length) {
       if (this.withdraw(storageWithEnergy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         this.moveTo(storageWithEnergy[0]);
+      }
+    // Otherwise get energy from containers
+    } else if (containersWithEnergy) {
+      if (this.withdraw(containersWithEnergy, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        this.moveTo(containersWithEnergy);
       }
     // If all else fails harvest some
     } else {
