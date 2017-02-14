eae7622b3d570243f0462b80c5a74d9637122938
Chris Boyle <chris.boyle@enernoc.com>
2017-02-14 18:43:48 -0500

    Handle branch in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,33 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-screeps');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.initConfig({
     secrets: grunt.file.readJSON('secrets.json'),
+    shell: {
+      gitBranch: {
+        command: 'git rev-parse --abbrev-ref HEAD',
+        options: {
+          callback: function(err, stdout, stderr, cb) {
+            gitBranch = stdout.trim();
+
+            if (gitBranch == 'master') {
+              grunt.config.set('gitBranch', 'default');
+            } else {
+              grunt.config.set('gitBranch', gitBranch);
+            }
+
+            cb();
+          },
+          stdout: false
+        }
+      }
+    },
     screeps: {
       options: {
         email: '<%= secrets.screeps.email %>',
         password: '<%= secrets.screeps.password %>',
-        branch: 'default',
+        branch: '<%= gitBranch %>',
         ptr: false
       },
       dist: {
@@ -15,4 +35,6 @@ module.exports = function(grunt) {
       }
     }
   });
+
+  grunt.registerTask('default', ['shell', 'screeps']);
 }
